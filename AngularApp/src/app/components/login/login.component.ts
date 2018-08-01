9ba7b7efd0177eb393b9d0cd6fe2f39dc60653a2
Saajith Zain <saajith1730@gmail.com>
2018-08-01 23:36:01 +0530

    Client Application basic

---
+++ AngularApp/src/app/components/login/login.component.ts
@@ -0,0 +1,48 @@
+import { Component, OnInit } from '@angular/core';
+import { NgForm } from '../../../../node_modules/@angular/forms';
+import { Router }  from '@angular/router'; 
+import { Http ,Headers} from '@angular/http'; 
+
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+
+  constructor(private router: Router, private http: Http) { }
+  private username:string;
+  private pswd:string;
+  ngOnInit() {
+  }
+
+  onSignUpClick(){
+    this.router.navigate(['/Signup']);
+  }
+
+  onLoginSubmit(form : NgForm,name){
+    let headers = new Headers({ 'Content-Type': 'application/json' })
+    let model:UserModel=new UserModel(); 
+    model.username=form.value.email;
+    model.password=form.value.password;
+    let body = JSON.stringify(model);
+  
+    this.http.post('http://localhost:3000/login',body,{headers: headers}).subscribe(
+      res=>{
+        console.log(res.json);
+      },
+      error=>{
+        console.log("error");
+      }
+    );
+  }
+
+ 
+
+}
+export class UserModel{
+    username: string;
+    password: string;
+    
+}
---
+++ AngularApp/src/app/components/page-not-found/pagenotfound-component.ts
@@ -0,0 +1,11 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-page-error',
+  template: '<h2> Page Not Found </h2>'
+ 
+})
+export class PageNotFoundComponent  {
+
+
+}
