28aa3b597821491c26780a3ffd97d1cfeb804056
Saajith <saajith@MacBooks-MacBook-Pro-2.local>
2018-09-05 14:56:30 +0530

    test

--- Server/src/User/auth.routes.js
+++ Server/src/User/auth.routes.js
@@ -21,17 +21,18 @@ router.all('/logout', (req, res) => {
     res.status(200).send({ auth: false, token: null });
 })
 
-// router.use((req, res, next) => {
-//     var token = req.headers['token'];
-//     if(!token) return res.status(401).send({
-//         auth: false,
-//         message: 'No token provided'
-//     });
-//     jwt.verify(token, authConfig.secret, (err, decoded)=>{
-//         if(err) return res.status(500)
-//             .send({auth: false, message: 'Failed to authenticate token'});
-//         next();
-//     })
-// });
+router.use((req, res, next) => {
+   
+    var token = req.headers['token'];
+    if(!token) return res.status(401).send({
+        auth: false,
+        message: 'No token provided'
+    });
+    jwt.verify(token, authConfig.secret, (err, decoded)=>{
+        if(err) return res.status(500)
+            .send({auth: false, message: 'Failed to authenticate token'});
+        next();
+    })
+});
 
 module.exports = router;
